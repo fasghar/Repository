09b5dcd197ca39a02efd9bc7e72e241c203bed09
Faizan Asghar <faizanasghar@Faizans-MacBook-Pro.local>
2019-08-02 20:49:05 +0100

    first commit

---
+++ TechTest/TestRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TechTest
+{
+    public class TestRepository
+    {
+        public TestRepository()
+        {
+        }
+    }
+}
---
+++ TechTest/TestStoreable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TechTest
+{
+    public class TestStoreable
+    {
+        public TestStoreable()
+        {
+        }
+    }
+}
